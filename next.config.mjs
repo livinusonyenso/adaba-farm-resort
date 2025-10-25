3f4a023cbfb6104bff1d72c0ae18c058d3cab792
livinusonynso <livinusonyenso@gmail.com>
2025-10-25 09:17:24 +0100

    disabled reactjs chart

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,21 @@
+import { fileURLToPath } from 'url';
+import path from 'path';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    turbo: {
+      resolveAlias: {
+        'react-is': path.dirname(fileURLToPath(import.meta.url)) + '/node_modules/react-is'
+      }
+    }
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
   images: {
     unoptimized: true,
   },
-}
+};
 
-export default nextConfig
+export default nextConfig;
